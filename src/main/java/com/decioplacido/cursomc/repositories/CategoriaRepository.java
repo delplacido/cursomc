13bfb9b7e7d0374c315ca8035548b6f969bf55b2
Decio Placido <delplacido@gmail.com>
2018-07-30 19:04:39 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/decioplacido/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.decioplacido.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.decioplacido.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
